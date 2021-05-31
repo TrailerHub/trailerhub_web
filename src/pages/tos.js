73cb809f6d1b12de58a6bc7b20d1b3e9ff56bd9b
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-05-31 15:39:00 -0500

    fixed pp and tos iframe

--- src/pages/tos.js
+++ src/pages/tos.js
@@ -3,7 +3,7 @@ import './iframeStyles.css';
 
 const ToS = () => {
     return (
-        <div class="container">
+        <div>
             <iframe class="responsive-iframe" title="Terms of Service" src="https://docs.google.com/document/d/e/2PACX-1vSp1mQDQq5KZZrwDW4Pbs1hmJDDI4nYsjM-epw_Q-kmUS0T1nuXfKPbbmlm1xfjDImfyw9yoLtuJGyu/pub?embedded=true"></iframe>
         </div>
     )
