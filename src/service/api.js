6e217953757c48a8718cc1a5707330fdbe497e72
Rodo <rodolfohansschmitt@gmail.com>
2021-10-28 16:55:55 -0500

    changed trailer page api to use production url

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const prodUrl = 'https://us-central1-share-d5fd6.cloudfunctions.net/';
+// const devUrl = 'https://us-central1-trailerhub-95c42.cloudfunctions.net/';
 const api = axios.create({
-    baseURL: 'https://us-central1-trailerhub-95c42.cloudfunctions.net/',
+    baseURL: prodUrl,
     timeout: 2000,
 });
 
